09305202038794c57b6b5accbfccc5431567ef33
circoIlnarda <93973100+circoIlnarda@users.noreply.github.com>
2021-11-10 11:15:33 +0100

    argomenti da linea di comando

---
+++ es9-11-21/es9-11-21/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+
+namespace es9_11_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Insert file address");
+            //string address = Console.ReadLine();
+            //string fileText = ReadFile(address);
+
+            //if (fileText == null)
+            //{
+            //    return;
+            //}
+
+            //Console.WriteLine("Save a New file or Rewrite it?");
+            //Console.WriteLine("Select N for a new file or R to Rewrite it");
+            //string newSelect = Console.ReadLine();
+
+            //Console.WriteLine("give me the word to find...");
+            //string wordToFind = Console.ReadLine();
+            //Console.WriteLine("give me the word to replace");
+            //string wordToReplace = Console.ReadLine();
+
+            //string modText = Replace(fileText, wordToFind, wordToReplace);
+            //WriteFile(modText, address, newSelect);
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+
+            string indirizzo = args[0];
+
+            string parolaDaCercare = args[1];
+
+            string parolaDaSostituire = args[2];
+
+            string sceltaNuovo = args[3];
+
+
+            string testoFile = ReadFile(indirizzo);
+
+            string testoModificato = Replace(testoFile, parolaDaCercare, parolaDaSostituire);
+
+            WriteFile(testoModificato, indirizzo, sceltaNuovo);
+        }
+
+        public static string Replace(string Text, string wordToFind, string wordToReplace)
+        {
+            string modText = Text.Replace(wordToFind, wordToReplace);
+            return modText;
+        }
+        public static string ReadFile(string address)
+        {
+            if (File.Exists(address))
+            {
+                try
+                {
+                    string fileText = File.ReadAllText(address);
+                    return fileText;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("File not avaiable");
+                    return null;
+                }
+            }
+            else
+            {
+                Console.WriteLine("file not exist");
+                return null;
+            }
+        }
+        public static void WriteFile(string textToSave, string address, string newSelect)
+        {
+            if (newSelect.Equals("N"))
+            {
+                string[] path = address.Split("\\");
+                path[path.Length - 1] = "copia.txt";
+                string newAddress = string.Join("\\", path);
+                try
+                {
+                    File.WriteAllText(newAddress, textToSave);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("ERROR. File not saved");
+                }
+            }
+        }
+    }
+}
--- esercizio9-11-21/Program.cs
+++ esercizio9-11-21/Program.cs
@@ -6,7 +6,58 @@ namespace esercizio9_11_21
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello pippo!");
+            Console.WriteLine("Dammi l'indirizzo del file");
+            string indirizzo = Console.ReadLine();
+
+            string testoFile = LeggiIlFile(indirizzo);
+
+            if (testoFile == null)
+            {
+                return;
+            }
+
+            Console.WriteLine("Vuoi un Nuovo file o vuoi Sovrascrivere il precedente?");
+            Console.WriteLine("Seleziona N per nuovo e S per sovrascrivere");
+            string sceltaNuovo = Console.ReadLine();
+
+            string testoModificato = Replace(testoFile, parolaDaCercare, parolaDaRimpiazzare);
+
+            ScriviIlFile(testoModificato, indirizzo, sceltaNuovo);
+            break;
+                case 2:
+                    Console.WriteLine("Se vuoi rendere maiusole tutte le lettere premi T"
+                                      + Environment.NewLine
+                                      + "se invece vuoi rendere maiuscole le lettere dopo il punto premi P");
+            string sceltaPunto = Console.ReadLine();
+
+            public static string Replace(string testo, string parolaDaCercare, string parolaDaRimpiazzare)
+            {
+                string testoModificato = testo.Replace(parolaDaCercare, parolaDaRimpiazzare);
+
+                return testoModificato;
+            }
+            public static string LeggiIlFile(string indirizzo)
+            {
+                if (File.Exists(indirizzo))
+                {
+                    try
+                    {
+                        string testoDelFile = File.ReadAllText(indirizzo);
+                        return testoDelFile;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("File non accessibile");
+                        return null;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Il file non esiste");
+                    return null;
+                }
+            }
+
         }
     }
 }
